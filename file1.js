8b3c3f39c3fa427f5efd50ca28389638893434aa
Malandiy Artem <artem.malandii@advantiss.com>
2022-11-21 14:17:51 +0200

    attempt 3 end

--- file1.js
+++ file1.js
@@ -1,1 +1,11 @@
 var file_1 = 0;
+
+playing
+
+function() {
+    console.log('Nice');
+}
+
+I [hope] it is 'working'
+
+start?
--- file2.js
+++ file2.js
@@ -1,1 +1,10 @@
 var file_2 = 1;
+
+attemp3
+function() {
+    console.log('Hello');
+}
+
+test
+
+I don't know';
