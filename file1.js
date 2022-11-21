bc905e365a1b73a1848e717a01f183f3d7b31d80
Malandiy Artem <artem.malandii@advantiss.com>
2022-11-21 13:48:20 +0200

    end

--- file1.js
+++ file1.js
@@ -1,1 +1,8 @@
 var file_1 = 0;
+Hello, thid is a test of recording
+And now I am on the first
+function () {
+    const a = 0;
+}
+
+Well, I think enought for now.
--- file2.js
+++ file2.js
@@ -1,1 +1,9 @@
 var file_2 = 1;
+I changed file to the second
+Let's check brackets'
+
+function() {
+    console.log('Bye');
+}
+
+Finished
