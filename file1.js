283238365e6347f93f1f0a14dd91115cd738bde5
Malandiy Artem <artem.malandii@advantiss.com>
2022-11-21 14:05:52 +0200

    sample 2

--- file1.js
+++ file1.js
@@ -1,8 +1,1 @@
 var file_1 = 0;
-Hello, thid is a test of recording
-And now I am on the first
-function () {
-    const a = 0;
-}
-
-Well, I think enought for now.
--- file2.js
+++ file2.js
@@ -1,9 +1,1 @@
 var file_2 = 1;
-I changed file to the second
-Let's check brackets'
-
-function() {
-    console.log('Bye');
-}
-
-Finished
