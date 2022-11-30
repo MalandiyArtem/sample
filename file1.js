6b84d50877821198efd2bb3e271da2f9b9a8770b
Malandiy Artem <artem.malandii@advantiss.com>
2022-11-30 19:51:39 +0200

    ff

--- file1.js
+++ file1.js
@@ -1,3 +1,1 @@
 var file_1 = 0;
-Hello this is a test record
-Now I am writing in the first file
--- file2.js
+++ file2.js
@@ -1,4 +1,1 @@
 var file_2 = 1;
-Now I am on the second file
-Let's look at it
-Maybe I should test brackets?
