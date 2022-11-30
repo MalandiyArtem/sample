c1ce8427d021841d4564ee83051e6d5f51b44a47
Malandiy Artem <artem.malandii@advantiss.com>
2022-11-30 19:08:32 +0200

    test

--- file1.js
+++ file1.js
@@ -1,8 +1,1 @@
 var file_1 = 0;
-Test #4
-hello?
-
-yes?()'''
-hello from test
-
-bye
--- file2.js
+++ file2.js
@@ -1,9 +1,1 @@
 var file_2 = 1;
-switvch
-yep?
-function ()(){
-    this.isFinite.normal
-}
-
-hello there
-well, bye
