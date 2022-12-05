744a5d027ad812cba96fbbb206c12f577fdf857f
Malandiy Artem <artem.malandii@advantiss.com>
2022-12-05 21:24:57 +0200

    end

--- file1.js
+++ file1.js
@@ -1,2 +1,30 @@
 var file_1 = 0;
 
+Typing without deleting within one file got it 
+
+Now test when I write
+On several lines
+Test Headersand test here
+
+
+
+Hello
+
+
+function test() {
+    console.log('Pasted content');
+}
+function test() {
+    console.log('Pasted content');
+}
+function test() {
+    
+}
+
+Typing and deleting in 
+
+Now test [quotes] (and) "brackets" [] ( ) '' ""
+
+And now typing with replacing content yesy deleting one q 
+
+And now deleting on the different line, line number 21 and continue typing emd end end
