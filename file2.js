d93ab831edf1c3e7ba38b42780410322f2fa4157
Malandiy Artem <artem.malandii@advantiss.com>
2022-12-14 22:06:03 +0200

    end

--- file2.js
+++ file2.js
@@ -1,5 +1,19 @@
 var file_2 = 1;
 
 
-end jsjsj
-j
+// end jsjsj
+// j
+// end jsjsj
+// j
+// end jsjsj
+// j
+// end jsjsj
+// j
+// end jsjsj
+// j
+// end jsjsj
+// j
+// end jsjsj
+// j
+// end jsjsj
+// j
