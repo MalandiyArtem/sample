88c7198f0d78c240a5efca58a3fc4ea81550455c
Malandiy Artem <artem.malandii@advantiss.com>
2022-12-16 15:34:15 +0200

    sad

--- file2.js
+++ file2.js
@@ -1,46 +1,13 @@
 q
 q
-
-
 q
 q
-qqqqqqqqq
 q
-qq
-
-
-
-va file_2 = 1;
-var array = [];
-var obj = {
-    text: 'Test',
-    value: 'value',
-    number: 12
-};
-
-obj.arra.number.array;
-console.log();
-
-
-w
-w
-
-w
-;;;;;;;;;
-
-// end jsjsj
-// j
-// end jsjsj
-// j
-// end jsjsj
-// j
-// end jsjsj
-// j
-// end jsjsj
-// j
-// end jsjsj
-// j
-// end jsjsj
-// j
-// end jsjsj
-// j
+q
+q
+q
+q
+q
+q
+q
+q
